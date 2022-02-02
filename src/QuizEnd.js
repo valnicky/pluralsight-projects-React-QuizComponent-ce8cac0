2f3185726114849ade94b25b39253555ddfe3112
valnicky <valery0007@gmail.com>
2022-02-02 22:31:38 +0100

    quizend

---
+++ src/QuizEnd.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class QuizEnd extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                Thanks for playing!
+                 </p>
+                 <a href=''>Rest Quiz</a>
+            </div>
+        );
+    }
+}
+
+export default QuizEnd
